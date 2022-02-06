f1befc0ff68227531ff6c4964de271fe3de9883b
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-06 21:26:19 +0400

    Update main.lua

--- lua/gpm/packages/base_extensions/main.lua
+++ lua/gpm/packages/base_extensions/main.lua
@@ -1,27 +1,65 @@
-local tonumber = tonumber
+local assert = assert
 local type = type
 
-local pbit = false
-local pbit_package = GPM.Loader.FindPackage("pBit")
-if pbit_package and (pbit_package.state == "loaded") then
-    pbit = true
-end
+local math_Round = math.Round
+local math_floor = math.floor
+local math_abs = math.abs
+local math_max = math.max
 
-local _Color = environment.saveFunc( "Color", Color )
-function Color(hex, g, b, a)
-    if (g == nil) and (b == nil) and (a == nil) then
-        local type = type( hex )
-        if (type == "string") then
-            hex = hex:gsub("#", "")
-            if (hex:len() == 3) then
-                return _Color( tonumber("0x" .. hex:sub(1, 1)) * 17, tonumber("0x" .. hex:sub(2, 2)) * 17, tonumber("0x" .. hex:sub(3, 3)) * 17 )
-            else
-                return _Color( tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6)) )
-            end
-        elseif (type == "number") and pbit then
-            return _Color( PLib.Vec4FromInt(hex) )
-        end
+--[[-------------------------------------------------------------------------
+	Angle improvements
+---------------------------------------------------------------------------]]
+
+do
+
+    local ANGLE = FindMetaTable("Angle")
+    function ANGLE:Floor()
+        self[1] = math_floor(self[1])
+        self[2] = math_floor(self[2])
+        self[3] = math_floor(self[3])
+        return self
     end
 
-    return _Color( hex, g, b, a )
+    function ANGLE:abs()
+        self[1] = math_abs(self[1])
+        self[2] = math_abs(self[2])
+        self[3] = math_abs(self[3])
+        return self
+    end
+
+end
+
+--[[-------------------------------------------------------------------------
+	Vector improvements
+---------------------------------------------------------------------------]]
+
+do
+
+    local VECTOR = FindMetaTable("Vector")
+    function VECTOR:Diameter(maxs)
+        return math_max( maxs[1] + math_abs(self[1]), maxs[2] + math_abs(self[2]), maxs[3] + math_abs(self[3]) )
+    end
+
+    function VECTOR:InBox(vec1, vec2)
+        return self[1] >= vec1[1] and self[1] <= vec2[1] and self[2] >= vec1[2] and self[2] <= vec2[2] and self[3] >= vec1[3] and self[3] <= vec2[3]
+    end
+
+    function VECTOR:Round(dec)
+        return Vector( math_Round(self[1], dec or 0), math_Round(self[2], dec or 0), math_Round(self[3], dec or 0) )
+    end
+
+    function VECTOR:Floor()
+        self[1] = math_floor(self[1])
+        self[2] = math_floor(self[2])
+        self[3] = math_floor(self[3])
+        return self
+    end
+
+    function VECTOR:Abs()
+        self[1] = math_abs(self[1])
+        self[2] = math_abs(self[2])
+        self[3] = math_abs(self[3])
+        return self
+    end
+
 end
